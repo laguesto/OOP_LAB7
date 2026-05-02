8f4bec48c4e1db7fd9a84567b7c8eac4371f2ab8
vasiaw <fvlog36@gmail.com>
2026-05-02 10:33:35 +0300

    OOP_LAB7_C#

---
+++ OOP_LAB7_C#/Program/ExceptionLab/ExceptionClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionLib
+{
+    public class ExceptionClass
+    {
+        private double _a;
+        private double _b;
+        private double _c;
+        private double _d;
+
+        public ExceptionClass(double a, double b, double c, double d)
+        {
+            this._a = a;
+            this._b = b;
+            this._c = c;
+            this._d = d;
+        }
+
+        public double Calculate()
+        {
+            double numerator = _a * _b / 4.0 - 1;
+            double underRoot = 41 - _d - _b * _a + _c;
+
+            if (underRoot < 0)
+            {
+                throw new Exception("underroot < 0");
+            }
+
+            double denominator = Math.Sqrt(underRoot);
+
+            if (denominator == 0)
+            {
+                throw new Exception("dividing by 0");
+            }
+
+            return numerator / denominator;
+        }
+    }
+}
---
+++ OOP_LAB7_C#/Program/Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using ExceptionLib;
+
+double a, b, c, d;
+
+Console.Write("Enter a, b, c, d: ");
+a = Convert.ToDouble(Console.ReadLine());
+b = Convert.ToDouble(Console.ReadLine());
+c = Convert.ToDouble(Console.ReadLine());
+d = Convert.ToDouble(Console.ReadLine());
+
+try
+{
+    ExceptionClass expr = new ExceptionClass(a, b, c, d);
+    double result = expr.Calculate();
+
+    Console.WriteLine("Result: " + result);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
